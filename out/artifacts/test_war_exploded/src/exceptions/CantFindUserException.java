e3ef0093ab34f9331cd053c1f24a1cebf9d3859b
campervcs <praidforum@mail.ru>
2016-11-16 16:36:00 +0300

    removed gitignored files

--- out/artifacts/test_war_exploded/src/exceptions/CantFindUserException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class CantFindUserException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Wrong username or password";
-    }
-}
--- out/artifacts/test_war_exploded/src/exceptions/UserExistException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-public class UserExistException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Username already exsist";
-    }
-}
